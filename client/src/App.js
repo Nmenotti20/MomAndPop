90bdfa03b7d9521470703c18e7875b27fb340c17
Brett Winterstrom <brettwinterstrom@yahoo.com>
2021-02-26 19:52:39 -0800

    added navbar and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
+import React from "react";
+import Footer from "./components/Footer";
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   );
 }
