352043c498f276c961d7343d5b60bb02ce39e010
Brett Winterstrom <brettwinterstrom@yahoo.com>
2021-03-06 13:45:00 -0800

    added a post component and styling

--- client/src/App.js
+++ client/src/App.js
@@ -8,19 +8,27 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import BizLogin from "./pages/BizLogin";
 import BizProfile from "./pages/BizProfile";
 import Header from "./components/Header";
+import Login from "./components/Login";
+import Feed from "./components/Feed";
 
 function App() {
+
   return (
-    <Router>
-      {/* <Navbar /> */}
-      < Header />
-      <Route path="/" exact component={UserLogin} />
-      <Route path="/settings" component={Settings} />
-      <Route path="/profile" component={Profile} />
-      <Route path="/businesslogin" component={BizLogin} />
-      <Route path="/businessprofile" component={BizProfile} />
-      <Footer />
-    </Router>
+    <div>
+
+      <Router>
+        {/* <Navbar /> */}
+        < Header />
+        <Feed />
+        <Route path="/" exact component={UserLogin} />
+        <Route path="/settings" component={Settings} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/businesslogin" component={BizLogin} />
+        <Route path="/businessprofile" component={BizProfile} />
+        <Footer />
+      </Router>
+      )
+    </div>
   );
 }
 
