3f8c69ad8ab147daca7e456cd4e30f008ce3f8e5
Nickolas Menotti <nmenotti@cox.net>
2021-03-09 20:58:38 -0800

    fixed responsive header, and added stars and comments to Business Cards

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -10,41 +10,46 @@ import twitterIcon from "../images/twitter.png"
 
 function Footer() {
     return (
-      <footer id="footer">
-        <div className="row">
-          <div className="col-lg-12">
-            {/* <a
-              className="gitHub"
-              href="https://github.com/Nmenotti20/MomAndPop"
-              target="_blank"
-              rel="noopener noreferrer">
-              <img src={gitHubIcon} alt="github icon" loading="lazy"></img>
-            </a> */}
-            <a
-              className="igIcon"
-              href="https://www.instagram.com/"
-              target="_blank"
-              rel="noopener noreferrer">
-              <img src={igIcon} alt="instagram icon" loading="lazy"></img>
-            </a>
-            <a
-              className="linkedInIcon"
-              href="https://www.linkedin.com/"
-              target="_blank"
-              rel="noopener noreferrer">
-              <img src={linkedIcon} alt="LinkedIN icon" loading="lazy"></img>
-            </a>
-            <a
-              className="twitterIcon"
-              href="https://twitter.com/"
-              target="_blank"
-              rel="noopener noreferrer">
-              <img src={twitterIcon} alt="Twitter icon" loading="lazy"></img>
-            </a>
-            {/* <input></input><button>Subscribe</button> */}
-          </div>
-        </div>
-      </footer>
+      <div classNameName="container">
+        {/* <div className="d-flex justify-content-center h-100"> */}
+
+          <footer id="footer">
+            <div className="row">
+              <div className="col-lg-12">
+                <a
+                  className="gitHub"
+                  href="https://github.com/Nmenotti20/MomAndPop"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  <img src={gitHubIcon} alt="github icon" loading="lazy"></img>
+                </a>
+                <a
+                  className="igIcon"
+                  href="https://www.instagram.com/"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  <img src={igIcon} alt="instagram icon" loading="lazy"></img>
+                </a>
+                <a
+                  className="linkedInIcon"
+                  href="https://www.linkedin.com/"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  <img src={linkedIcon} alt="LinkedIN icon" loading="lazy"></img>
+                </a>
+                <a
+                  className="twitterIcon"
+                  href="https://twitter.com/"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  <img src={twitterIcon} alt="Twitter icon" loading="lazy"></img>
+                </a>
+                <input></input><button>Subscribe</button>
+              </div>
+            </div>
+          </footer>
+        {/* </div> */}
+      </div>
     );
 }
 
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -3,7 +3,8 @@ import "./style.css";
 import BusinessOwner from '../images/Petersons_Donughts_Img.png';
 import { Card, CardColumns } from "react-bootstrap";
 import { FaStar } from 'react-icons/fa';
-import { StarRating } from '../components/StarRating';
+import StarRating from '../components/StarRating'
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
 
 
 const Landing = () => {
@@ -21,7 +22,15 @@ const Landing = () => {
                     </Card.Body>
                     <Card.Body>
                         <Card.Link href="#">Business Website Link</Card.Link><br /><br />
-                        <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar />
+                        <div className="post_options">
+                            <div className="post_option">
+                                <StarRating />
+                            </div>
+                            <div className="post_option">
+                                <ChatBubbleOutlineIcon />
+                                <p>Comment</p>
+                            </div>
+                        </div>
                     </Card.Body>
                     </Card>
                     <Card className="card" style={{ height: 'auto', width: 'auto' }}>
@@ -34,7 +43,15 @@ const Landing = () => {
                     </Card.Body>
                     <Card.Body>
                         <Card.Link href="#">Business Website Link</Card.Link><br /><br />
-                        <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar />
+                        <div className="post_options">
+                            <div className="post_option">
+                                <StarRating />
+                            </div>
+                            <div className="post_option">
+                                <ChatBubbleOutlineIcon />
+                                <p>Comment</p>
+                            </div>
+                        </div>
                     </Card.Body>
                     </Card>
                     <Card className="card" style={{ height: 'auto', width: 'auto' }}>
@@ -47,7 +64,15 @@ const Landing = () => {
                     </Card.Body>
                     <Card.Body>
                         <Card.Link href="#">Business Website Link</Card.Link><br /><br />
-                        <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar />
+                        <div className="post_options">
+                            <div className="post_option">
+                                <StarRating />
+                            </div>
+                            <div className="post_option">
+                                <ChatBubbleOutlineIcon />
+                                <p>Comment</p>
+                            </div>
+                        </div>
                     </Card.Body>
                     </Card>
                     <Card className="card" style={{ height: 'auto', width: 'auto' }}>
@@ -60,7 +85,15 @@ const Landing = () => {
                     </Card.Body>
                     <Card.Body>
                         <Card.Link href="#">Business Website Link</Card.Link><br /><br />
-                        <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar />
+                        <div className="post_options">
+                            <div className="post_option">
+                                <StarRating />
+                            </div>
+                            <div className="post_option">
+                                <ChatBubbleOutlineIcon />
+                                <p>Comment</p>
+                            </div>
+                        </div>
                     </Card.Body>
                     </Card>
                     <Card className="card" style={{ height: 'auto', width: 'auto' }}>
@@ -73,7 +106,15 @@ const Landing = () => {
                     </Card.Body>
                     <Card.Body>
                         <Card.Link href="#">Business Website Link</Card.Link><br /><br />
-                        <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar />
+                        <div className="post_options">
+                            <div className="post_option">
+                                <StarRating />
+                            </div>
+                            <div className="post_option">
+                                <ChatBubbleOutlineIcon />
+                                <p>Comment</p>
+                            </div>
+                        </div>
                     </Card.Body>
                     </Card>
                     <Card className="card" style={{ height: 'auto', width: 'auto' }}>
@@ -86,7 +127,15 @@ const Landing = () => {
                     </Card.Body>
                     <Card.Body>
                         <Card.Link href="#">Business Website Link</Card.Link><br /><br />
-                        <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar />
+                        <div className="post_options">
+                            <div className="post_option">
+                                <StarRating />
+                            </div>
+                            <div className="post_option">
+                                <ChatBubbleOutlineIcon />
+                                <p>Comment</p>
+                            </div>
+                        </div>
                     </Card.Body>
                     </Card>
                     <Card className="card" style={{ height: 'auto', width: 'auto' }}>
@@ -99,7 +148,15 @@ const Landing = () => {
                     </Card.Body>
                     <Card.Body>
                         <Card.Link href="#">Business Website Link</Card.Link><br /><br />
-                        <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar />
+                        <div className="post_options">
+                            <div className="post_option">
+                                <StarRating />
+                            </div>
+                            <div className="post_option">
+                                <ChatBubbleOutlineIcon />
+                                <p>Comment</p>
+                            </div>
+                        </div>
                     </Card.Body>
                     </Card>
                     <Card className="card" style={{ height: 'auto', width: 'auto' }}>
@@ -112,7 +169,15 @@ const Landing = () => {
                     </Card.Body>
                     <Card.Body>
                         <Card.Link href="#">Business Website Link</Card.Link><br /><br />
-                        <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar />
+                        <div className="post_options">
+                            <div className="post_option">
+                                <StarRating />
+                            </div>
+                            <div className="post_option">
+                                <ChatBubbleOutlineIcon />
+                                <p>Comment</p>
+                            </div>
+                        </div>
                     </Card.Body>
                     </Card>
                     <Card className="card" style={{ height: 'auto', width: 'auto' }}>
@@ -125,7 +190,15 @@ const Landing = () => {
                     </Card.Body>
                     <Card.Body>
                         <Card.Link href="#">Business Website Link</Card.Link><br /><br />
-                        <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar />
+                        <div className="post_options">
+                            <div className="post_option">
+                                <StarRating />
+                            </div>
+                            <div className="post_option">
+                                <ChatBubbleOutlineIcon />
+                                <p>Comment</p>
+                            </div>
+                        </div>
                     </Card.Body>
                     </Card>
                 </CardColumns>     
