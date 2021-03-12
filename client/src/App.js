a8770999424cb30fe1987dea31b81e7e2d68d8d5
ezemog1996 <enrique_g_gomez@hotmail.com>
2021-03-12 16:06:19 -0500

    made sure we check they're uploading an image on BizRegister and took out some console.logs

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "./components/Footer";
 import NewUser from "./pages/NewUser";
 import UserLogin from "./pages/UserLogin";
