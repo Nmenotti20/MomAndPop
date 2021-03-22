ce303f55b70eec5d3b0a283a1965f6105a72edfa
ezemog1996 <enrique_g_gomez@hotmail.com>
2021-03-22 16:55:01 -0400

    reconfiguring multer for aws s3

--- config/middleware/multer.js
+++ config/middleware/multer.js
@@ -1,15 +1,34 @@
+const aws = require('aws-sdk');
 const multer = require('multer');
+const multerS3 = require('multer-s3');
 const { v4: uuidv4 } = require('uuid');
+const path = require('path');
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads');
-    },
-    filename: (req, file, cb) => {
-        const { originalname } = file;
-        cb(null, `${uuidv4()}-${originalname}`);
-    }
+const s3 = new aws.S3({ apiVersion: '2006-03-01' });
+
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'mompopbucket',
+        metadata: (req, file, cb) => {
+            cb(null, { fieldName: file.fieldname })
+        },
+        key: (req, file, cb) => {
+            const ext = path.extname(file.originalname);
+            cb(null, `${uuidv4()} ${ext}`)
+        }
+    })
 })
-const upload = multer({ storage });
+
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, 'uploads');
+//     },
+//     filename: (req, file, cb) => {
+//         const { originalname } = file;
+//         cb(null, `${uuidv4()}-${originalname}`);
+//     }
+// })
+// const upload = multer({ storage });
 
 module.exports = upload;
